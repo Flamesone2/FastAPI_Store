37a339892c810d8a045875646a7dce221f53719e
Flamesone2 <sergbasov.com@gmail.com>
2025-04-04 23:20:10 +0300

    Modify keycloak start script

--- keycloak/keycloak-start.sh
+++ keycloak/keycloak-start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/bin/bash
 
 minikube start
 
@@ -10,5 +10,14 @@ echo "Waiting for pods to be ready..."
 kubectl wait --for=condition=Ready pod -l app=postgres --timeout=300s
 kubectl wait --for=condition=Ready pod -l app=keycloak --timeout=300s
 
-echo "Keycloak URL:"
-minikube service keycloak --url --https=false
+# Получаем URL Keycloak
+KEYCLOAK_URL=$(minikube service keycloak --url --https=false)
+
+echo "Waiting for Keycloak to be ready..."
+while ! curl -s "$KEYCLOAK_URL/auth/realms/master" > /dev/null; do
+    echo "Keycloak is not ready yet. Retrying in 10 seconds..."
+    sleep 10
+done
+
+echo "Keycloak is ready!"
+echo "Keycloak URL: $KEYCLOAK_URL"
