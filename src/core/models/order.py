708dd21b88b4d8555173a09f52341455add81777
Flamesone2 <sergbasov.com@gmail.com>
2025-04-07 20:06:24 +0300

    Create Order, OrderItem model

--- src/core/models/order.py
+++ src/core/models/order.py
@@ -1,14 +1,12 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, ForeignKey, String, Float
 from typing import List
-from mixins import IdIntPkMixin
-from base import Base
+from .base import Base
+from .mixins import IdIntPkMixin
 
 
 class Order(IdIntPkMixin, Base):
-    user_id: Mapped[str] = mapped_column(
-        String, index=True
-    )  # ID пользователя из Keycloak
+    user_id: Mapped[str] = mapped_column(String, index=True)
     status: Mapped[str] = mapped_column(String, default="pending")
 
     items: Mapped[List["OrderItem"]] = relationship("OrderItem", back_populates="order")
@@ -19,5 +17,4 @@ class OrderItem(IdIntPkMixin, Base):
     product_id: Mapped[int] = mapped_column(Integer, ForeignKey("products.id"))
     quantity: Mapped[int] = mapped_column(Integer)
     price: Mapped[float] = mapped_column(Float)
-
     order: Mapped["Order"] = relationship("Order", back_populates="items")
