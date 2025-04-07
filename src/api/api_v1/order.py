25571d15d2c573564b949349c856ee60633bf987
Flamesone2 <sergbasov.com@gmail.com>
2025-04-08 00:14:00 +0300

    Add user_orders

--- src/api/api_v1/order.py
+++ src/api/api_v1/order.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from core.config import settings
 from core.models.db_helper import db_helper
 from core.schemas.order import OrderCreate, OrderRead
-from crud.order import create_order
+from crud.order import create_order, get_orders_by_user
+from api.api_v1.dependencies.user import get_current_user
+
 
 router = APIRouter(prefix=settings.api.v1.order, tags=["Orders"])
 
@@ -12,6 +13,16 @@ router = APIRouter(prefix=settings.api.v1.order, tags=["Orders"])
 @router.post("/", response_model=OrderRead)
 async def create_new_order(
     order_create: OrderCreate,
+    user_id: str = Depends(get_current_user),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
+    order_create.user_id = user_id
     return await create_order(session, order_create)
+
+
+@router.get("/user_orders", response_model=list[OrderRead])
+async def get_user_orders(
+    user_id: str = Depends(get_current_user),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    return await get_orders_by_user(session, user_id)
--- src/crud/order.py
+++ src/crud/order.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from sqlalchemy import select
 from core.models import Order, OrderItem
 from core.schemas.order import OrderCreate, OrderRead
 
@@ -19,3 +19,12 @@ async def create_order(session: AsyncSession, order_create: OrderCreate) -> Orde
     await session.commit()
     await session.refresh(order)
     return OrderRead.model_validate(order)
+
+
+async def get_orders_by_user(
+    session: AsyncSession,
+    user_id: str,
+) -> list[Order]:
+    stmt = select(Order).where(Order.user_id == user_id).order_by(Order.id)
+    result = await session.scalars(stmt)
+    return result.all()
