33c1a26cf120eb529df19993a666b50966b177b3
Flamesone2 <sergbasov.com@gmail.com>
2025-04-07 20:33:01 +0300

    plug routers

--- src/api/api_v1/__init__.py
+++ src/api/api_v1/__init__.py
@@ -4,8 +4,11 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .auth_keycloak import router as auth_router
-
+from .product import router as product_router
+from .order import router as order_router
 
 router = APIRouter(prefix=settings.api.v1.prefix)
 
 router.include_router(auth_router)
+router.include_router(product_router)
+router.include_router(order_router)
---
+++ src/crud/__init__.py
@@ -0,0 +1,8 @@
+__all__ = (
+    "create_product",
+    "get_all_products",
+    "create_order",
+)
+
+from .product import get_all_products, create_product
+from .order import create_order
